f8d1b27a1d3ceb51854777bbc9cfa1672738aba5
YoshinoriHashizume <yoshinori_hashizume@marueikogyo.jp>
2025-04-04 13:51:44 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Yoshinori Hashizume on 2025/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
